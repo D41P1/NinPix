c57586bf5c94aecbd3370612a455fca3ed487113
D41P1 <156718659+D41P1@users.noreply.github.com>
2024-03-10 13:06:21 +0000

    PseudoCodeV2

--- src/ReplicatedFirst/InputHandler.lua
+++ src/ReplicatedFirst/InputHandler.lua
@@ -1,9 +1,28 @@
 --[[
-    Current Inputs: 
-    Movement: W A S D
-    inputtable = {} -- could be a submodule
+    Current Inputs: {
+        Movement: W A S D
+    }
+    ModuleScript Format:
+    local CharacterEvents -- events made from the Character Controller script   
+    local MovementEvent: Unreliable -- CharacterEvents.MovementEvent  
+    
+    local InputHandler = {}
+    inputtable = {
+        W = function()
+            -- calculate the Direction: Vector3 they wanna move using the x and z of the camera
+            -- write direction to buffer
+            MovementEvent:FireServer(buffer)
+        end
+    } -- could be a submodule
+
     userinput connect function(key) 
         if not  inputtable[key] then return end
         inputtable[key]()
     end)
+
+    function InputHandler:GiveConnections()
+        -- so you can disconnect Keybinds if need be from another table
+        return inputtable    
+    end)
+    return InputHandler
 ]]
